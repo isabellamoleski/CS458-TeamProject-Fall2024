8e0ac45afc6cc410e42912715644a20b1db58aa5
curbina <cruz.urbin.28@gmail.com>
2024-10-02 10:33:33 -0600

    initial commit

--- app/src/main/java/com/example/habittrackerapp/MainActivity.java
+++ app/src/main/java/com/example/habittrackerapp/MainActivity.java
@@ -13,8 +13,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-// Github push test comment - disregard
-
 public class MainActivity extends AppCompatActivity {
 
     // Define main variables
